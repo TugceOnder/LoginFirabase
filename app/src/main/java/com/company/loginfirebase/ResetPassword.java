ec39d99ad82914f62a60508dc7f877adfd00f9e9
TugceOnder <tugceonder775@gmail.com>
2021-01-26 00:57:49 +0300

    Update ResetPassword.java

--- app/src/main/java/com/company/loginfirebase/ResetPassword.java
+++ app/src/main/java/com/company/loginfirebase/ResetPassword.java
@@ -12,15 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 
 public class ResetPassword extends AppCompatActivity {
 
-    Button mForgetPaword;
-    FirebaseAuth firebaseAuth;
+    Button mForgetPasword;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     EditText resetMailText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,46 +30,24 @@ public class ResetPassword extends AppCompatActivity {
         setContentView(R.layout.activity_reset_password);
 
 
-        mForgetPaword=findViewById(R.id.forgetPasswordButton);
+        mForgetPasword=findViewById(R.id.forgetPasswordButton);
         resetMailText=findViewById(R.id.sendEmailText);
 
-        mForgetPaword.setOnClickListener(new View.OnClickListener() {
+        mForgetPasword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //  FirebaseAuth.getInstance().signOut();
-             //   startActivity(new Intent(getApplicationContext(),ResetPassword.class));
-                 EditText resetMail = new EditText(v.getContext());
-                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
-                passwordResetDialog.setTitle("Reset Password ? ");
-                passwordResetDialog.setMessage("Enter Your Email to  Received Reset Link");
-                passwordResetDialog.setView(resetMail);
-                passwordResetDialog.setPositiveButton("EVET", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+              String userEmail=resetMailText.getText().toString();
+              firebaseAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                  @Override
+                  public void onComplete(@NonNull Task<Void> task) {
+                      if(task.isSuccessful()){
+                          Toast.makeText(ResetPassword.this,"Email'nize şifre yolladı ",Toast.LENGTH_LONG).show();
+                      }
 
-                        // e postayı kaldır ve sıfırlama mesajı gönder
+                  }
+              });
+              finish();
 
-                        String mail = resetMailText.getText().toString();
-                        firebaseAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Toast.makeText(ResetPassword.this, "Reset Link Sent to Your Email", Toast.LENGTH_LONG).show();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(ResetPassword.this, "ERROR! RESET LINK IS NOT SENT"+e.getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
-                });
-                passwordResetDialog.setNegativeButton("HAYIR", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // dialoğu kapat
-                    }  });
-
-                passwordResetDialog.create().show();
             }
         });
 
